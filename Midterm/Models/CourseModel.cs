41ed499236c33571c6d2fa6002f7ad7e5d88ec3a
michealpwalls <michealpwalls@gmail.com>
2015-07-02 15:23:39 -0400

    Added Data Annotations for validation

--- Midterm/Models/CourseModel.cs
+++ Midterm/Models/CourseModel.cs
@@ -1,15 +1,20 @@
 namespace Midterm.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
     using System.Linq;
 
     public class CourseModel
     {
         public int ID { get; set; }
+        [Required]
         public string CourseCode { get; set; }
+        [Required]
         public string CourseName { get; set; }
+        [Required]
         public string CourseDescription { get; set; }
+        [Required]
         public float CourseCost { get; set; }
     }
 
